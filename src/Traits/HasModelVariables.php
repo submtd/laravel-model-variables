2da6f7541b1a8b182139d43e8d9ca77a2f472284
Steve Harmeyer <harmeyersteve@gmail.com>
2019-02-12 10:18:08 -0700

    updates

--- src/Traits/HasModelVariables.php
+++ src/Traits/HasModelVariables.php
@@ -33,6 +33,9 @@ trait HasModelVariables
 
     public function getVariable(string $index)
     {
+        if (!$this->exists) {
+            return null;
+        }
         if (!$variable = $this->variables()->where('index', $index)->first()) {
             return null;
         }
@@ -41,11 +44,23 @@ trait HasModelVariables
 
     public function setVariable(string $index, string $value)
     {
+        if (!$this->exists) {
+            return null;
+        }
         if (!$variable = $this->variables()->where('index', $index)->first()) {
             $variable = $this->variables()->create(['index' => $index]);
         }
         $variable->value = $value;
         $variable->save();
+        return true;
+    }
+
+    public function deleteVariable(string $index)
+    {
+        if (!$this->exists) {
+            return null;
+        }
+        $this->variables()->where('index', $index)->delete();
         return true;
     }
 }
